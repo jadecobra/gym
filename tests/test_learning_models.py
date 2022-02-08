5c8eaba96f7e2655b15cb07af390dd5870e382bb
jadecobra <jakeitegsy@yahoo.com>
2022-02-07 19:32:51 -0600

    add pythonPackager

--- tests/test_learning_models.py
+++ tests/test_learning_models.py
@@ -2,29 +2,32 @@ from unittest import TestCase, main
 
 
 def learning_model(expectations, reality):
-    if reality < expectations:
-        return reality + 1
-    return expectations + 1
+    while reality < expectations:
+        reality += expectations - reality
+    return reality + 1
 
 
 class TestInfiniteLearningModel(TestCase):
 
     def test_learning_model_when_expectations_are_greater_than_reality(self):
-        "When expectations are greater than reality we add to our reality until it meets expectations"
+        "When our expectations are greater than reality we add to our reality until it exceeds expectations"
         expectations = 1
         reality = 0
-        self.assertEqual(expectations, learning_model(expectations, reality))
 
-    def test_learning_model_when_expectations_are_less_than_reality(self):
-        "When expectations are less than reality we raise our expectations"
-        expectations = 0
-        reality = 1
         self.assertEqual(expectations + 1, learning_model(expectations, reality))
 
+    def test_learning_model_when_expectations_are_less_than_reality(self):
+        "When our expectations are less than reality we raise our expectations until it exceeds reality"
+        expectations = 0
+        reality = 1
+
+        self.assertEqual(reality + 1, learning_model(expectations, reality))
+
     def test_learning_model_when_expectations_are_equal_to_reality(self):
-        "When expectations match reality we raise our expectations"
+        "When our expectations match reality we raise our expectations"
         expectations = 1
         reality = 1
+
         self.assertEqual(expectations + 1, learning_model(expectations, reality))
 
 if __name__ == "__main__":
