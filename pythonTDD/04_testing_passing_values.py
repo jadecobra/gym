73c991672a9c5067f6bddc63f013d9ca11579bc2
Jake Itegboje <jakeitegsy@yahoo.com>
2022-02-15 09:25:41 -0600

    fix indentation

--- pythonTDD/04_testing_passing_values.py
+++ pythonTDD/04_testing_passing_values.py
@@ -5,11 +5,11 @@ from telephone import Telephone
 class TestPassingValues(TestCase):
 
 	def test_text_messages(self):
-		self.assertEqual(Telephone().text('hello'), 'i received this message: hello')
-		self.assertEqual(Telephone().text('yes'), 'i received this message: yes')
+            self.assertEqual(Telephone().text('hello'), 'i received this message: hello')
+            self.assertEqual(Telephone().text('yes'), 'i received this message: yes')
 
 	def test_phone_calls(self):
-		self.assertEqual(
-            Telephone().call(1234567890),
-            'did you mean to call: 1234567890'
-        )
+            self.assertEqual(
+                Telephone().call(1234567890),
+                'did you mean to call: 1234567890'
+            )
