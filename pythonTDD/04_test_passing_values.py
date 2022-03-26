74fa673560e5f1b06211c72a87961c48d3000d0b
jadecobra <jakeitegsy@yahoo.com>
2022-03-25 19:28:20 -0500

    fix imports

--- pythonTDD/04_test_passing_values.py
+++ pythonTDD/04_test_passing_values.py
@@ -1,15 +1,21 @@
-from unittest import TestCase
-from telephone import Telephone
+import unittest
+import telephone
 
 
-class TestPassingValues(TestCase):
+class TestPassingValues(unittest.TestCase):
 
-	def test_text_messages(self):
-            self.assertEqual(Telephone().text('hello'), 'i received this message: hello')
-            self.assertEqual(Telephone().text('yes'), 'i received this message: yes')
+    def test_text_messages(self):
+        self.assertEqual(
+            telephone.Telephone().text('hello'),
+            'I received this message: hello'
+        )
+        self.assertEqual(
+            telephone.Telephone().text('yes'),
+            'I received this message: yes'
+        )
 
-	def test_phone_calls(self):
-            self.assertEqual(
-                Telephone().call(1234567890),
-                'did you mean to call: 1234567890'
-            )
+    def test_phone_calls(self):
+        self.assertEqual(
+            telephone.Telephone().call(1234567890),
+            'Helloooo! did you mean to call: 1234567890'
+        )
