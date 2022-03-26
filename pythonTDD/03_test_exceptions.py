5b87b54a24c5c49d70e9f0f56564c787ed3ff01b
jadecobra <jakeitegsy@yahoo.com>
2022-03-25 19:26:20 -0500

    fix imports

--- pythonTDD/03_test_exceptions.py
+++ pythonTDD/03_test_exceptions.py
@@ -1,90 +1,84 @@
-from unittest import TestCase
-from exceptions import (
-	raises_exception_error, exception_handler,
-	raises_exception, succeeding_function,
-	another_exception_handler, always_returns, error_catcher, zero_division_catcher, zero_division_catcher_with_finally
-)
+import unittest
+import exceptions
 
+class TestExceptionHandling(unittest.TestCase):
 
-class TestExceptionHandling(TestCase):
+    def test_catching_exceptions_in_tests(self):
+        with self.assertRaises(ZeroDivisionError):
+            1/0
 
-	def test_catching_exceptions_in_tests(self):
-		with self.assertRaises(ZeroDivisionError):
-			1/0
+    def test_catching_exceptions(self):
+        with self.assertRaises(Exception):
+            exceptions.raises_exception_error()
 
-	def test_catching_exceptions(self):
-		with self.assertRaises(Exception):
-			raises_exception_error()
+    def test_catches_things_that_fail_0(self):
+        self.assertEqual(
+            exceptions.exception_handler(exceptions.raises_exception),
+            'failed'
+        )
 
-	# def test_all_errors_inherit_from_exception_class(self):
-	# 	self.assertIsInstance(ConnectionError, BrokenPipeError)
+    def test_catches_things_that_dont_fail_0(self):
+        self.assertEqual(
+            exceptions.exception_handler(exceptions.succeeding_function),
+            'succeeded'
+        )
 
-	def test_catches_things_that_fail_0(self):
-		self.assertEqual(
-			exception_handler(raises_exception),
-			'failed'
-		)
+    def test_catches_things_that_fail_1(self):
+        self.assertEqual(
+            exceptions.another_exception_handler(exceptions.raises_exception),
+            'failed'
+        )
 
-	def test_catches_things_that_dont_fail_0(self):
-		self.assertEqual(
-			exception_handler(succeeding_function),
-			'succeeded'
-		)
+    def test_catches_things_that_dont_fail_1(self):
+        self.assertEqual(
+            exceptions.another_exception_handler(exceptions.succeeding_function),
+            'succeeded'
+        )
 
-	def test_catches_things_that_fail_1(self):
-		self.assertEqual(
-			another_exception_handler(raises_exception),
-			'failed'
-		)
+    def test_finally_always_returns(self):
+        self.assertEqual(
+            exceptions.always_returns(exceptions.succeeding_function),
+            'always_returns_this'
+        )
+        self.assertEqual(
+            exceptions.always_returns(exceptions.raises_exception),
+            'always_returns_this'
+        )
 
-	def test_catches_things_that_dont_fail_1(self):
-		self.assertEqual(
-			another_exception_handler(succeeding_function),
-			'succeeded'
-		)
+    def test_catching_explicit_errors(self):
+        self.assertEqual(
+            exceptions.zero_division_catcher(ZeroDivisionError),
+            'caught_zero_division_error'
+        )
+        with self.assertRaises(TypeError):
+            exceptions.zero_division_catcher(TypeError)
 
-	def test_finally_always_returns(self):
-		self.assertEqual(
-			always_returns(succeeding_function),
-			'always_returns_this'
-		)
-		self.assertEqual(
-			always_returns(raises_exception),
-			'always_returns_this'
-		)
+        with self.assertRaises(AttributeError):
+            exceptions.zero_division_catcher(AttributeError)
 
-	def test_catching_explicit_errors(self):
-		self.assertEqual(
-			zero_division_catcher(ZeroDivisionError),
-			'caught_zero_division_error'
-		)
-		with self.assertRaises(TypeError):
-			zero_division_catcher(TypeError)
-		with self.assertRaises(AttributeError):
-			zero_division_catcher(AttributeError)
-		with self.assertRaises(Exception):
-			zero_division_catcher(Exception)
+        with self.assertRaises(Exception):
+            exceptions.zero_division_catcher(Exception)
 
-	def test_catching_explicit_errors_with_finally(self):
-		self.assertEqual(
-			zero_division_catcher_with_finally(ZeroDivisionError),
-			'caught_zero_division_error'
-		)
-		self.assertEqual(
-			zero_division_catcher_with_finally(TypeError),
-			'caught_nothing'
-		)
-		self.assertEqual(
-			zero_division_catcher_with_finally(AttributeError),
-			'caught_nothing'
-		)
-		self.assertEqual(
-			zero_division_catcher_with_finally(Exception),
-			'caught_nothing'
-		)
+    def test_catching_explicit_errors_with_finally(self):
+        self.assertEqual(
+            exceptions.zero_division_catcher_with_finally(ZeroDivisionError),
+            'caught_zero_division_error'
+        )
+        self.assertEqual(
+            exceptions.zero_division_catcher_with_finally(TypeError),
+            'caught_nothing'
+        )
+        self.assertEqual(
+            exceptions.zero_division_catcher_with_finally(AttributeError),
+            'caught_nothing'
+        )
+        self.assertEqual(
+            exceptions.zero_division_catcher_with_finally(Exception),
+            'caught_nothing'
+        )
 
-	def test_catching_specific_errors(self):
-		self.assertEqual(
-			str(error_catcher(ZeroDivisionError)),
-			str(ZeroDivisionError())
-		)
+    def test_catching_specific_errors(self):
+        self.assertEqual(
+            str(exceptions.error_catcher(ZeroDivisionError)),
+            str(ZeroDivisionError())
+        )
