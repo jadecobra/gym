020497d7666b884063c361f324d813ce9b456a94
jadecobra <jakeitegsy@yahoo.com>
2022-03-25 19:49:42 -0500

    add data structures test

---
+++ pythonTDD/00_test_data_structures.py
@@ -0,0 +1,56 @@
+import unittest
+
+
+class TestDataStructures(unittest.TestCase):
+
+    def test_what_is_none(self):
+        self.assertIsNotNone(None)
+        self.assertIsNone(True)
+        self.assertIsNone(False)
+        self.assertIsNone(1)
+        self.assertIsNone(0)
+        self.assertIsNone(-1)
+        self.assertIsNone('')
+        self.assertIsNone('text')
+        self.assertIsNone(())
+        self.assertIsNone((1, 2, 3, 'n'))
+        self.assertIsNone([])
+        self.assertIsNone([1, 2, 3, 'n'])
+        self.assertIsNone({})
+        self.assertIsNone({1, 2, 3, 'n'})
+        self.assertIsNone(dict())
+        self.assertIsNone(
+            {
+                'a': 1,
+                'b': 2,
+                'c': 3,
+                'n': 'n',
+            }
+        )
+
+    def test_what_is_false(self):
+        self.assertTrue(None)
+        self.assertTrue(False)
+        self.assertTrue(0)
+        self.assertTrue('')
+        self.assertTrue(())
+        self.assertTrue([])
+        self.assertTrue({})
+        self.assertTrue(dict())
+
+    def test_what_is_true(self):
+        self.assertFalse(True)
+        self.assertFalse(1)
+        self.assertFalse(-1)
+        self.assertFalse('text')
+        self.assertFalse((1, 2, 3, 'n'))
+        self.assertFalse([1, 2, 3, 'n'])
+        self.assertFalse({1, 2, 3, 'n'})
+        self.assertFalse(
+            {
+                'a': 1,
+                'b': 2,
+                'c': 3,
+                'n': 'n',
+            }
+        )
--- pythonTDD/README.md
+++ pythonTDD/README.md
@@ -1,10 +1,18 @@
-Learn Test Driven Development with Python
+# Learn Python through Test Driven Development
 
-00 Learn to Setup a Test Driven Python Project
-00 Python Data Structures - Learn the primitives
-01 Functions
-02 Classes
-03 Exceptions
-04 Passing Values
-05 Dictionaries
-06 Truth Table
+- 00 Learn to Setup a Test Driven Python Project
+- 00 Python Data Structures - Learn the primitives
+- 01 Functions
+- 02 Classes
+- 03 Exceptions
+- 04 Passing Values
+- 05 Dictionaries
+- 06 Truth Table
+
+## How to start testing
+```
+python3 -m venv .venv
+.venv/bin/activate
+pip install sniffer macFSevents
+sniffer
+```
