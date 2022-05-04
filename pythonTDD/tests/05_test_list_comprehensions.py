db6eebba590eae96dd28d08dcfaabe93f7ac0d0e
jadecobra <jakeitegsy@yahoo.com>
2022-05-04 16:30:41 -0500

    update tests

--- pythonTDD/tests/05_test_list_comprehensions.py
+++ pythonTDD/tests/05_test_list_comprehensions.py
@@ -3,39 +3,101 @@ import list_comprehensions
 
 
 class TestListComprehensions(unittest.TestCase):
+    def test_creating_lists_from_a_collection(self):
+        collection = range(10)
+        a_list = []
+        for element in collection:
+            a_list.append(element)
 
-    def test_list_comprehensions(self):
+        self.assertEqual(a_list, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
         self.assertEqual(
-            [x for x in range(10) if x % 2 == 0],
-            [0, 2, 4, 6, 8]
+            list(collection),
+            a_list,
         )
         self.assertEqual(
-            [x for x in range(10) if x % 2 == 0 and x % 3 == 0],
-            [0, 6]
+            list_comprehensions.make_a_list(collection),
+            a_list,
+        )
+
+    def test_creating_lists_with_a_for_loop(self):
+        collection = range(10)
+        a_list = []
+        for element in collection:
+            a_list.append(element)
+
+        self.assertEqual(a_list, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+        self.assertEqual(
+            list_comprehensions.for_loops(collection),
+            a_list,
+        )
+
+    def test_creating_lists_with_a_list_comprehension(self):
+        collection = range(10)
+        a_list = []
+        for element in collection:
+            a_list.append(element)
+
+        self.assertEqual(a_list, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+        self.assertEqual(
+            [element for element in collection],
+            a_list,
         )
         self.assertEqual(
-            [x for x in range(10) if x % 2 == 0 or x % 3 == 0],
-            [0, 2, 4, 6, 8, 3, 9]
+            list_comprehensions.list_comprehension(collection),
+            a_list,
+        )
+
+    def test_list_comprehensions_with_conditions(self):
+        collection = range(10)
+        even_numbers = []
+        for element in collection:
+            if element % 2 == 0:
+                even_numbers.append(element)
+
+        self.assertEqual(even_numbers, [0, 2, 4, 6, 8])
+        self.assertEqual(
+            [element for element in collection if element % 2 == 0],
+            a_list,
         )
         self.assertEqual(
-            [x for x in range(10) if x % 2 == 0 and x % 3 == 0 or x % 5 == 0],
-            [0, 6, 5]
+            list_comprehensions.with_conditions(collection),
+            a_list,
+        )
+
+    def test_list_comprehensions_with_and_conditions(self):
+        collection = range(10)
+        result = []
+        for element in collection:
+            if element % 2 == 0 and element % 3 == 0:
+                result.append(element)
+
+        self.assertEqual(result, [0, 6])
+        self.assertEqual(
+            [
+                element
+                for element in collection
+                if element % 2 == 0 and element % 3 == 0
+            ],
+            result,
         )
         self.assertEqual(
-            [x for x in range(10) if x % 2 == 0 and x % 3 == 0 or x % 5 == 0 and x % 7 == 0],
-            [0, 6, 5, 7]
+            list_comprehensions.with_and_conditions(collection),
+            result,
+        )
+
+    def test_list_comprehensions_with_or_conditions(self):
+        collection = range(10)
+        result = []
+        for element in collection:
+            if element % 2 == 0 or element % 3 == 0:
+                result.append(element)
+
+        self.assertEqual(result, [0, 2, 4, 6, 8, 3, 9])
+        self.assertEqual(
+            [element for element in collection if element % 2 == 0 or element % 3 == 0],
+            result,
         )
         self.assertEqual(
-            [x for x in range(10) if x % 2 == 0 and x % 3 == 0 or x % 5 == 0 and x % 7 == 0 and x % 11 == 0],
-            [0, 6, 5, 7, 11]
+            list_comprehensions.with_or_conditions(collection),
+            result,
         )
-        self.assertEqual(
-            [x for x in range(10) if x % 2 == 0 and x % 3 == 0 or x % 5 == 0 and x % 7 == 0 and x % 11 == 0 and x % 13 == 0],
-            [0, 6, 5, 7, 11, 13]
-        )
-        self.assertEqual(
-            [x for x in range(10) if x % 2 == 0 and x % 3 == 0 or x % 5 == 0 and x % 7 == 0 and x % 11 == 0 and x % 13 == 0 and x % 17 == 0],
-            [0, 6, 5, 7, 11, 13, 17]
-        )
-        self.assertEqual(
-            [x for x in range(10) if x
