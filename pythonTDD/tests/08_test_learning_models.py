d994c8b90795e4677bd5ca522575a49db1d87525
jadecobra <jakeitegsy@yahoo.com>
2022-05-21 17:21:29 -0500

    description

--- pythonTDD/tests/08_test_learning_models.py
+++ pythonTDD/tests/08_test_learning_models.py
@@ -4,21 +4,21 @@ import learning
 
 class TestInfiniteLearningModel(unittest.TestCase):
     def test_learning_model_when_expectations_are_greater_than_reality(self):
-        "When our expectations are greater than reality we add to our reality until it exceeds expectations"
+        "When expectations exceed reality, add to reality until it exceeds expectations"
         expectations = 1
         reality = 0
 
         self.assertEqual(expectations + 1, learning.model(expectations, reality))
 
     def test_learning_model_when_expectations_are_less_than_reality(self):
-        "When our expectations are less than reality we raise our expectations until it exceeds reality"
+        "When expectations exceed reality, add to expectations until it exceeds reality"
         expectations = 0
         reality = 1
 
         self.assertEqual(reality + 1, learning.model(expectations, reality))
 
     def test_learning_model_when_expectations_are_equal_to_reality(self):
-        "When our expectations match reality we raise our expectations"
+        "When expectations equal reality, raise expectations"
         expectations = 1
         reality = 1
 
