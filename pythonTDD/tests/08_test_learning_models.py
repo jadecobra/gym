b501173a4011033c1dd503f6d51463b5808ba9d7
jadecobra <jakeitegsy@yahoo.com>
2022-05-21 18:39:26 -0500

    remove random

--- pythonTDD/tests/08_test_learning_models.py
+++ pythonTDD/tests/08_test_learning_models.py
@@ -1,25 +1,24 @@
 "How do I know I am learning?"
 import unittest
-import random
 import learning
 
 
 class TestInfiniteLearningModel(unittest.TestCase):
     def test_learning_model_when_expectations_greater_than_reality(self):
         "When expectations exceed reality, update reality to exceed expectations"
-        reality = random.random()
+        reality = 0
         expectations = reality + 1
         self.assertGreater(learning.model(expectations, reality), expectations)
 
     def test_learning_model_when_expectations_less_than_reality(self):
         "When reality exceeds expectations, update expectations to exceed reality"
-        reality = random.random()
+        reality = 1
         expectations = reality - 1
         self.assertGreater(learning.model(expectations, reality), reality)
 
     def test_learning_model_when_expectations_equal_to_reality(self):
         "When expectations equal reality, raise expectations"
-        reality = random.random()
+        reality = 1
         expectations = reality
         self.assertGreater(learning.model(expectations, reality), expectations)
 
