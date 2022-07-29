c252009eb41970c4356efca15ae265e84b949e65
jadecobra <jakeitegsy@yahoo.com>
2022-07-29 00:48:11 -0500

    format

--- python_tdd/tests/test_hypothesis/tests/test_encoding.py
+++ python_tdd/tests/test_hypothesis/tests/test_encoding.py
@@ -5,11 +5,18 @@ import hypothesis
 
 
 class TestEncoding(unittest.TestCase):
+    
     @hypothesis.given(hypothesis.strategies.text())
     def test_encoding(self, a_string):
-        self.assertEqual(encoding.decode(encoding.encode(a_string)), a_string)
+        self.assertEqual(
+            encoding.decode(encoding.encode(a_string)), 
+            a_string
+        )
 
     @hypothesis.given(a_string=hypothesis.strategies.text())
     @hypothesis.example(a_string="")
     def test_encoding_with_example(self, a_string):
-        self.assertEqual(encoding.decode(encoding.encode(a_string)), a_string)
+        self.assertEqual(
+            encoding.decode(encoding.encode(a_string)), 
+            a_string
+        )
