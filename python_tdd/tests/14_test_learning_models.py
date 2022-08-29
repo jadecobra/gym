6e30660766b4cbae1c0d7ca416de58426acb795a
jadecobra <jakeitegsy@yahoo.com>
2022-08-28 19:39:00 -0500

    rearrange

--- python_tdd/tests/14_test_learning_models.py
+++ python_tdd/tests/14_test_learning_models.py
@@ -10,20 +10,29 @@ class TestInfiniteLearningModel(unittest.TestCase):
         increase reality until it is greater than expectations'''
         reality = 0
         expectations = reality + 1
-        self.assertGreater(learning.model(expectations, reality), expectations)
+        self.assertGreater(
+            learning.model(expectations, reality),
+            expectations
+        )
 
     def test_learning_model_when_expectations_less_than_reality(self):
         '''When expectations are less than reality,
         increase expectations until they are greater than reality'''
         reality = 1
         expectations = reality - 1
-        self.assertGreater(learning.model(expectations, reality), reality)
+        self.assertGreater(
+            learning.model(expectations, reality),
+            reality
+        )
 
     def test_learning_model_when_expectations_equal_to_reality(self):
         '''When expectations equal reality, increase expectations'''
         reality = 1
         expectations = reality
-        self.assertGreater(learning.model(expectations, reality), expectations)
+        self.assertGreater(
+            learning.model(expectations, reality),
+            expectations
+        )
 
 
 if __name__ == "__main__":
