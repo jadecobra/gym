a01ab57daa0f6746bb50bf492830a134f154ae53
jadecobra <jakeitegsy@yahoo.com>
2023-03-29 16:48:37 -0500

    mock output

--- python_tdd/tests/test_lambda_power_tuner/lambda_power_tuner.py
+++ python_tdd/tests/test_lambda_power_tuner/lambda_power_tuner.py
@@ -5,3 +5,17 @@ def create_configuration(arn=None, memory=None, number=None, payload=None):
         "num": number,
         "payload": {} if not payload else payload
     }
+
+def optimize(configuration):
+    return {
+        "results": {
+            "power": "128",
+            "cost": 0.0000002083,
+            "duration": 2.906,
+            "stateMachine": {
+                "executionCost": 0.00045,
+                "lambdaCost": 0.0005252,
+                "visualization": "https://lambda-power-tuning.show/#<encoded_data>"
+            }
+        }
+    }
