9480688b12f36e62bd9cf1d0435ccb0f3bee9ae0
jadecobra <jakeitegsy@yahoo.com>
2023-03-29 16:43:20 -0500

    extract variables

--- python_tdd/tests/test_lambda_power_tuner/tests/test_lambda_power_tuner.py
+++ python_tdd/tests/test_lambda_power_tuner/tests/test_lambda_power_tuner.py
@@ -1,26 +1,41 @@
 import unittest
 import jadecobra.toolkit
+import jadecobra.aws_lambda
 import lambda_power_tuner
 
 class TestLambdaPoweTuner(jadecobra.toolkit.TestCase):
+
+    region = "us-east-1"
+    account = "012345678901"
+    function_name = "lambda_function_name"
+
+    def lambda_function_arn(self):
+        return jadecobra.aws_lambda.get_arn(
+            name=self.function_name,
+            region=self.region,
+            account=self.account,
+        )
+
+    def test_arn(self):
+        self.assertEqual(
+            self.lambda_function_arn(),
+            f"arn:aws:lambda:{self.region}:{self.account}:function:{self.function_name}"
+        )
 
     def test_power_tuner_interface(self):
         number = 50
         payload = {}
         memory = [128, 256, 512, 1024]
-        region = "us-east-1"
-        account = "012345678901"
-        function_name = "lambda_function_name"
-        lambda_function_arn = f"arn:aws:lambda:{region}:{account}:function:{lambda_function_name}"
+
         self.assertEqual(
             lambda_power_tuner.create_configuration(
-                arn=lambda_function_arn,
+                arn=self.lambda_function_arn(),
                 memory=memory,
                 number=number,
                 payload=payload
             ),
             {
-                "lambdaARN": lambda_function_arn,
+                "lambdaARN": self.lambda_function_arn(),
                 "powerValues": memory,
                 "num": number,
                 "payload": payload
