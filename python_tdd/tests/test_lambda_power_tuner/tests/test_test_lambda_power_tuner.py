0503e99e01d754b82c2cb4b66d8bfb97b4df0fd6
jadecobra <jakeitegsy@yahoo.com>
2023-03-29 15:25:12 -0500

    add test

--- python_tdd/tests/test_lambda_power_tuner/tests/test_test_lambda_power_tuner.py
+++ python_tdd/tests/test_lambda_power_tuner/tests/test_test_lambda_power_tuner.py
@@ -3,7 +3,21 @@ import jadecobra.toolkit
 
 class TestLambdaPoweTuner(jadecobra.toolkit.TestCase):
 
-    def test_failure(self):
+    def test_power_tuner_interface(self):
+        self.assertEqual(
+            lambda_power_tuner.create_input(
+                lambda_arn="your-lambda-function-arn",
+                powerValues=[128, 256, 512, 1024],
+                num=50,
+                payload={}
+            ),
+            {
+                "lambdaARN": "your-lambda-function-arn",
+                "powerValues": [128, 256, 512, 1024],
+                "num": 50,
+                "payload": {}
+            }
+        )
         self.assertTrue(True)
 
     def test_z_commit(self):
