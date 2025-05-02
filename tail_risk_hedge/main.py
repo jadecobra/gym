ad7945fce2417978b96bf323829233b96bb7822f
jadecobra <jakeitegsy@yahoo.com>
2025-05-02 18:14:08 +0000

    add options cache

--- tail_risk_hedge/main.py
+++ tail_risk_hedge/main.py
@@ -1,12 +1,10 @@
 import tail_risk_hedge
 
-
 def print_header(scenario, delimiter_length=60):
     print('\n')
     print('-'*delimiter_length)
     print(f'\t{scenario} scenario')
     print('-'*delimiter_length)
-
 
 def print_metrics(metrics):
     for key, value in metrics.items():
@@ -41,16 +39,11 @@ print_metrics(
 )
 
 data_provider = tail_risk_hedge.YahooFinanceDataProvider(
-    seed=42, cache_file="price_cache.pkl"
+    seed=42, cache_file="price_cache.pkl",
+    put_options_cache_file="put_options_cache.pkl"
 )
 
 print_header("dynamic stable")
-stable_metrics = tail_risk_hedge.calculate_portfolio_metrics(
-    portfolio_value=100000,
-    insurance_ratio=0.01,
-    **data_provider.generate_scenario("stable")
-)
-
 print_metrics(
     tail_risk_hedge.calculate_portfolio_metrics(
         portfolio_value=100000,
