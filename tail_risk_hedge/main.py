4bab9337b820a5298a26df6b45c099b047b6072e
jadecobra <jakeitegsy@yahoo.com>
2025-05-02 19:41:29 +0000

    remove static scenarios to reflect dynamic real scenarios only

--- tail_risk_hedge/main.py
+++ tail_risk_hedge/main.py
@@ -10,32 +10,6 @@ def print_metrics(metrics):
     for key, value in metrics.items():
         print(f"{key.replace('_', ' ')}: {value}")
 
-print_header("stable")
-print_metrics(
-    tail_risk_hedge.calculate_portfolio_metrics(
-        portfolio_value=100000,
-        insurance_ratio=0.01,
-        price_at_start=500,
-        price_at_end=525,
-        strike_price=400,
-        option_price=1,
-        expiry_date="2025-07-18"
-    )
-)
-
-print_header("crash")
-print_metrics(
-    tail_risk_hedge.calculate_portfolio_metrics(
-        portfolio_value=100000,
-        insurance_ratio=0.01,
-        price_at_start=500,
-        price_at_end=350,
-        strike_price=400,
-        option_price=1,
-        expiry_date="2025-07-18"
-    )
-)
-
 data_provider = tail_risk_hedge.YahooFinanceDataProvider(
     seed=42, cache_file="price_cache.pkl", put_options_cache_file="put_options_cache.pkl"
 )
